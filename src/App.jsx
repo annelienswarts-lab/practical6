e8d49528abaa0fd81be7adf2f4c9cb2732e555d8
izayahizaaks-art <izayahizaaks@gmail.com>
2026-02-09 15:41:33 +0200

    initial project
    react add ons

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='app'>
+      <h1>hello world</h1>
+
+      <button>click me please</button>
+      
+      
+    </div>
+  )
+}
+
+export default App
